17ee4d99ee0b7a6b937ba406c24a23769a086d8e
Aaron Munger <aaron.munger@gmail.com>
2016-02-24 21:57:53 -0800

    added procfile

---
+++ firmware/postTemp/postTemp.ino
@@ -0,0 +1,114 @@
+// Include the ESP8266 WiFi library. (Works a lot like the
+// Arduino WiFi library.)
+#include <ESP8266WiFi.h>
+
+//////////////////////
+// WiFi Definitions //
+//////////////////////
+const char WiFiSSID[] = "laserChloe";
+const char WiFiPSK[] = "princessface";
+
+const char http_site[] = "52.33.86.126";
+
+/////////////////////
+// Pin Definitions //
+/////////////////////
+const int LED_PIN = 5; // Thing's onboard, green LED
+const int ANALOG_PIN = A0; // The only analog pin on the Thing
+const int DIGITAL_PIN = 12; // Digital pin to be read
+
+
+/////////////////
+// Post Timing //
+/////////////////
+const unsigned long postRate = 3000;
+unsigned long lastPost = 0;
+
+void connectWiFi()
+{
+  byte ledStatus = LOW;
+
+  // Set WiFi mode to station (as opposed to AP or AP_STA)
+  WiFi.mode(WIFI_STA);
+
+  // WiFI.begin([ssid], [passkey]) initiates a WiFI connection
+  // to the stated [ssid], using the [passkey] as a WPA, WPA2,
+  // or WEP passphrase.
+  WiFi.begin(WiFiSSID, WiFiPSK);
+
+  // Use the WiFi.status() function to check if the ESP8266
+  // is connected to a WiFi network.
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    // Blink the LED
+    digitalWrite(LED_PIN, ledStatus); // Write LED high/low
+    ledStatus = (ledStatus == HIGH) ? LOW : HIGH;
+
+    // Delays allow the ESP8266 to perform critical tasks
+    // defined outside of the sketch. These tasks include
+    // setting up, and maintaining, a WiFi connection.
+    delay(100);
+    // Potentially infinite loops are generally dangerous.
+    // Add delays -- allowing the processor to perform other
+    // tasks -- wherever possible.
+  }
+}
+
+void initHardware()
+{
+  Serial.begin(9600);
+  pinMode(DIGITAL_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  // Don't need to set ANALOG_PIN as input, 
+  // that's all it can be.
+}
+
+int postTemp()
+{
+  // LED turns on when we enter, it'll go off when we 
+  // successfully post.
+  digitalWrite(LED_PIN, HIGH);
+
+  WiFiClient client;
+  if (!client.connect(http_site, 3000)) 
+  {
+    // If we fail to connect, return 0.
+    return 0;
+  }
+  // If we successfully connected, print our Phant post:
+  client.print("GET /temp/");
+  client.print(String(analogRead(A0)));
+  client.println(" HTTP/1.1");
+  client.print("Host: ");
+  client.println(http_site);
+  client.println("Connection: close");
+  client.println();
+
+  delay(500);
+
+  // Before we exit, turn the LED off.
+  digitalWrite(LED_PIN, LOW);
+
+  return 1; // Return success
+}
+
+void setup() 
+{
+  initHardware();
+  connectWiFi();
+  digitalWrite(LED_PIN, HIGH);
+}
+
+void loop() 
+{
+  if (lastPost + postRate <= millis())
+  {
+    if (postTemp())
+      lastPost = millis();
+    else
+      delay(100);    
+  }
+}
+
+
