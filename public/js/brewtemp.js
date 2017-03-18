9f56be771b2fc3aca8ec9fbe909a67eb2d561ca1
mungera <aaron.munger@parivedasolutions.com>
2017-03-18 09:47:06 -0700

    converted to temp

--- public/js/brewtemp.js
+++ public/js/brewtemp.js
@@ -4,21 +4,29 @@ $(function (){
 	var socket = io.connect();
 	var series = new TimeSeries();
 
-	socket.on('reading', function (reading){
+	function convertTemp(voltage) {
+		const m = 0.25;
+		const b = -4.66667;
 
-		$('#current-reading').html(reading.temp);
-		series.append(reading.time, reading.temp);
+		var result = voltage * m + b;
+		return Math.round(result);
+	}
+
+	socket.on('reading', function (reading){
+		var convertedTemp = convertTemp(reading.temp);
+		$('#current-reading').html(convertedTemp);
+		series.append(reading.time, convertedTemp);
 	});
 
 	function myYRangeFunction(range) {
-		var min = 0;
-		var max = 1024;
+		var min = 50;
+		var max = 220;
 		return {min: min, max: max};
 	};
 
-	var chart = new SmoothieChart({millisPerPixel:100,
+	var chart = new SmoothieChart({millisPerPixel:900,
 		scaleSmoothing:0.115,
-		grid:{millisPerLine:6000,verticalSections:20},
+		grid:{millisPerLine:60000,verticalSections:17},
 		//timestampFormatter:SmoothieChart.timeFormatter,
 		yRangeFunction:myYRangeFunction});
 
